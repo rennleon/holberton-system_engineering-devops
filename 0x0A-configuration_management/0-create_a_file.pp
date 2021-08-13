fdb5bb661fcdf68ee7f39db43aded54821e3325b
rennleon <2829@holbertonschool.com>
2021-08-13 16:55:14 +0000

    Add comment to puppet manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
+# This manifest creates a file named 'holberton' in 'tmp' dir
+
 file { 'holberton':
-  ensure => 'present',
-  path => '/tmp/holberton',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => 'present',
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
