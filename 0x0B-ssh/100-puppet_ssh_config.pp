b0ebe7a10a5d63424d92ece6db1b5cd3a7500d34
rennleon <2829@holbertonschool.com>
2021-08-14 21:09:21 +0000

    Fix syntax

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,15 +6,15 @@ file {'Configuration File':
   path   => '/etc/ssh/ssh_config'
 }
 
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config'
-  line   => '  PasswordAuthentication no'
-}
-
 file_line { 'Declare identity file':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config'
+  path   => '/etc/ssh/ssh_config',
   line   => '  IdentityFile ~/.ssh/holberton'
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '  PasswordAuthentication no'
 }
 
