c16d83f6f93218c6dccd084b2cc7512d2f95b60f
rennleon <2829@holbertonschool.com>
2021-08-24 02:34:28 +0000

    Refactor manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,50 +1,14 @@
 # This manifest configures an nginx server
 
-$defaultconfig = "# Default server configuration
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	add_header X-Served-By ${hostname};
-
-	root /var/www/html;
-	index index.html index.htm;
-
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-
-	error_page 404 /404.html;
-	location =/404.html {
-		root /var/www/html;
-		internal;
-	}
-}
-"
-
 package { 'nginx':
   ensure  => 'installed',
 }
 
-file { 'Create default index.html':
-  ensure  => 'present',
-  name    => 'index.html',
-  path    => '/var/www/html/index.html',
-  content => "Holberton School is cool!\n"
-}
-
-file { 'Create default 404.html':
-  ensure  => 'present',
-  name    => '404.html',
-  path    => '/var/www/html/404.html',
-  content => "Ceci n'est pas une page\n"
-}
-
-file { 'Create nginx configuration file':
-  ensure  => 'present',
-  name    => 'default',
-  path    => '/etc/nginx/sites-available/default',
-  content => $defaultconfig
+file_line { 'Add header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen \[::\]:80 default_server;',
+  line   => "\n\tadd_header X-Served-By \$hostname;"
 }
 
 service { 'nginx':
