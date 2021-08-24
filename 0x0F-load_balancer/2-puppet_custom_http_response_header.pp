78ba4d22e67c94bc86696972089414aaa1d2d4fb
rennleon <2829@holbertonschool.com>
2021-08-24 02:58:21 +0000

    Add command dependency

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,10 +5,11 @@ package { 'nginx':
 }
 
 file_line { 'Add header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen \[::\]:80 default_server;',
-  line   => "\n\tadd_header X-Served-By \$hostname;"
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen \[::\]:80 default_server;',
+  line    => "\n\tadd_header X-Served-By \$hostname;",
+  require => Package['nginx']
 }
 
 service { 'nginx':
