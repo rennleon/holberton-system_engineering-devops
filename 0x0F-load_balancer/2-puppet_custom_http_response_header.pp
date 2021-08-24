43a5dfcac5d2dce976e2b9b1b07872d8fcfe303a
rennleon <2829@holbertonschool.com>
2021-08-24 03:03:22 +0000

    Add dependency order to puppet manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,15 +4,14 @@ package { 'nginx':
   ensure  => 'installed',
 }
 
-file_line { 'Add header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen \[::\]:80 default_server;',
-  line    => "\n\tadd_header X-Served-By \$hostname;",
-  require => Package['nginx']
+-> file_line { 'Add header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen \[::\]:80 default_server;',
+  line   => "\n\tadd_header X-Served-By \$hostname;"
 }
 
-service { 'nginx':
+-> service { 'nginx':
   ensure  => 'running',
   require => Package['nginx']
 }
