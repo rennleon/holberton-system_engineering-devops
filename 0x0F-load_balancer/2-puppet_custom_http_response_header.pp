65415f7f737b75c9b4a8c845f313466daae935de
rennleon <2829@holbertonschool.com>
2021-08-24 03:41:34 +0000

    Refactor code to start config file with default configuration

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,27 @@
 # This manifest configures an nginx server
 
+$config = "# Default server configuration
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	add_header X-Served-By \$hostname;
+
+	root /var/www/html;
+	index index.html index.htm;
+
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+
+	error_page 404 /404.html;
+	location =/404.html {
+		root /var/www/html;
+		internal;
+	}
+}
+"
+
 exec { 'update packages':
   command => '/usr/bin/apt update'
 }
@@ -8,11 +30,10 @@ exec { 'update packages':
   ensure  => 'installed',
 }
 
--> file_line { 'Add header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen \[::\]:80 default_server;',
-  line   => "\n\tadd_header X-Served-By \$hostname;"
+-> file { 'Create config file':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  content => $config
 }
 
 -> service { 'nginx':
