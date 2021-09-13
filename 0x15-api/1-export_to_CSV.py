0d86a0525c890277562adaa2f51836027a12c5ab
rennleon <2829@holbertonschool.com>
2021-09-13 17:45:51 +0000

    Create a Python script to make a request to a REST API and then export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module makes a request to an api and displays the results to stdout"""
+import csv
+from sys import argv
+import requests as rq
+
+url = 'https://jsonplaceholder.typicode.com/users/{}/{}'
+if __name__ == '__main__':
+    _id = argv[1]
+    u = rq.get(url.format(_id, '')).json()
+    todos = rq.get(url.format(_id, 'todos')).json()
+
+    data = []
+    for task in todos:
+        data.append([u['id'], u['username'], task['completed'], task['title']])
+
+    filename = '{}.csv'.format(u['id'])
+    with open(filename, 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(data)
