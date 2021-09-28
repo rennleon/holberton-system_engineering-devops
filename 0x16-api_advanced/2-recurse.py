3744b2403a5687434b14f392f5160af8f6d016ac
rennleon <2829@holbertonschool.com>
2021-09-28 19:49:34 +0000

    Fix infinite loop bug

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,7 @@ def append_posts(hot_list=[], posts=[], ind=0):
     """Add a post to the list"""
     if ind >= len(posts):
         return
-    post = posts[i].get('data', {})
+    post = posts[ind].get('data', {})
     title = post.get('title', '')
     hot_list.append(title)
     append_posts(hot_list, posts, ind + 1)
@@ -18,7 +18,7 @@ def append_posts(hot_list=[], posts=[], ind=0):
 def recurse(subreddit, hot_list=[], params={}):
     """This function fetches a reddit and returns a list with post
     titles for a reddit account"""
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'New agent 1.0'}
 
     res = rq.get(url.format(subreddit), headers=headers, params=params,
                  allow_redirects=False)
@@ -34,11 +34,14 @@ def recurse(subreddit, hot_list=[], params={}):
         return None
 
     updated_params = {
-        'after': body.get('after', ''),
-        'count': body.get('dist', 0),
+        'after': body.get('after', None),
+        'count': body.get('dist', 0) + params.get('count', 0),
         'limit': 100
     }
 
+    if updated_params.get('after') is None:
+        return hot_list
+
     append_posts(hot_list, body.get('children', []), 0)
 
-    recurse(subreddit, hot_list, params=updated_params)
+    return recurse(subreddit, hot_list, params=updated_params)
