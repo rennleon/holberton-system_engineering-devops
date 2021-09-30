756cefd5196fa42c291b31bcef0e65e62975e52c
rennleon <2829@holbertonschool.com>
2021-09-30 03:06:58 +0000

    Add description to manifest

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
+# This manifest fixes a bug on a web server
+
 exec { 'Fix php reference extension':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php' ,
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => 'bin'
 }
