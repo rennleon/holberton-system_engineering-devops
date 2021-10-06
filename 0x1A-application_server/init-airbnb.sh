748b35222e619274be6b1486b18cd91cfc3ca89b
rennleon <2829@holbertonschool.com>
2021-10-06 18:29:16 +0000

    Create a script to init basic services to expose airbnb project

---
+++ 0x1A-application_server/init-airbnb.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This script initializes services to expose airbnb
+
+cd /home/ubuntu/AirBnB_clone_v2
+tmux new-session -d 'gunicorn --bind 0.0.0.0:5000 web_flask.0-hello_route:app'
+tmux new-session -d 'gunicorn --bind 0.0.0.0:5001 web_flask.6-number_odd_or_even:app'
+
+cd /home/ubuntu/AirBnB_clone_v3
+export HBNB_ENV=test
+export HBNB_MYSQL_USER=hbnb_test
+export HBNB_MYSQL_PWD=hbnb_test_pwd
+export HBNB_MYSQL_HOST=localhost
+export HBNB_MYSQL_DB=hbnb_test_db
+export HBNB_TYPE_STORAGE=db
+tmux new-session -d 'gunicorn --bind 0.0.0.0:5002 api.v1.app:app'
